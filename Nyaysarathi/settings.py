f4e3583236f6699fa7ae503fd140791de4eeef2a
adarsh <adarshmaurya9415@gmail.com>
2023-11-22 13:31:59 +0530

    modified settings - added chatbot app

--- Nyaysarathi/settings.py
+++ Nyaysarathi/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatbot.apps.ChatbotConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
