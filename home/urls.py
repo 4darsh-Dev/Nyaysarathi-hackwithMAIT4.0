91fb379906aedc190cca259af56888b544f61582
adarsh <adarshmaurya9415@gmail.com>
2023-11-22 13:50:39 +0530

    updated app with urls patterns and views, functions for displaying web pages

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,13 @@ from home import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact , name="contact"),
+    path("services/", views.services , name="services"),
+    
+    path("login/",views.loginUser, name="loginUser"),
+    path("signup/", views.signupUser, name="signupUser"),
+    path("logout/", views.logoutUser, name="logout"),
 
+    
 ]
